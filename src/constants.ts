ef4f740b55bd091ca5645625c5611ba4e3960219
LassazVegaz <lasindunuwanga@gmail.com>
2023-10-15 16:54:59 +0530

    EOL added as a keyword in constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const KEYWORDS = ["say", "love", "as", "a"] as const;
+export const KEYWORDS = ["say", "love", "as", "a", "\n"] as const;
 
 export const VARIABLE_TYPES = ["string", "number"] as const;
 
