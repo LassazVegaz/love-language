802712899ae22df1219d8a70dfc46fc01b8655b6
LassazVegaz <lasindunuwanga@gmail.com>
2023-10-16 00:07:24 +0530

    made it working 💀🔫

--- src/passer.ts
+++ src/passer.ts
@@ -20,7 +20,8 @@ export default class Passer {
    * Execute say function.
    *
    * Print from current token to end of the line token. This will throw an error if any of the
-   * tokens is not a string, number or a defined variable.
+   * tokens is not a string, number or a defined variable. This will move `pos` to the end of the
+   * line.
    */
   private say() {
     while (this.token.type !== "keyword" || this.token.value !== "EOL") {
@@ -38,7 +39,7 @@ export default class Passer {
 
   /**
    * Save a variable. This will throw an error if the current token is not a variable name or if
-   * the next tokens are not "as a" followed by a variable type.
+   * the next tokens are not "as a", variable type and EOL. This will move `pos` to the end of the line.
    */
   private saveVariable() {
     // get variable name
@@ -71,6 +72,41 @@ export default class Passer {
 
     // save the variable
     this.variables[variableName] = variableValue;
+
+    // move to EOL
+    this.move();
+    if (this.token.type !== "keyword" || this.tokens[this.pos].value !== "EOL")
+      throw new Error(`Expected end of line but got ${this.token.value}`);
+  }
+
+  private changeVariableValue() {
+    // get variable name
+    if (this.token.type !== "keyword")
+      throw new Error(`Expected a variable name but got ${this.token.value}`);
+    const variableName = this.token.value;
+    if (!(variableName in this.variables))
+      throw new Error(`Variable ${variableName} is not defined`);
+    this.move();
+
+    // next one should be "is"
+    if (this.token.type !== "keyword" || this.token.value !== "is")
+      throw new Error(`Expected "is" but got ${this.token.value}`);
+    this.move();
+
+    // next one should be a value
+    if (this.tokens[this.pos].type !== "string")
+      throw new Error(
+        `Expected a value but got ${this.tokens[this.pos].value}`
+      );
+    const variableValue = this.tokens[this.pos].value;
+
+    // save the variable
+    this.variables[variableName] = variableValue;
+
+    // move to EOL
+    this.move();
+    if (this.token.type !== "keyword" || this.tokens[this.pos].value !== "EOL")
+      throw new Error(`Expected end of line but got ${this.token.value}`);
   }
 
   /**
@@ -91,13 +127,25 @@ export default class Passer {
 
     while (this.pos < this.tokens.length) {
       if (this.token.type === "keyword") {
+        // say function
         if (this.token.value === "say") {
           this.move();
           this.say();
-        } else if (this.token.value === "love") {
+        }
+
+        // variable declaration
+        else if (this.token.value === "love") {
           this.move();
           this.saveVariable();
-        } else {
+        }
+
+        // change variable value
+        else if (this.token.value in this.variables) {
+          this.changeVariableValue();
+        }
+
+        // nothing matched
+        else {
           throw new Error(`Invalid keyword ${(this.token as any).value}`);
         }
       } else {
