3319fff654dd8f2476c7e7efa1cb7a706eddc049
LassazVegaz <lasindunuwanga@gmail.com>
2023-10-15 16:47:49 +0530

    default var values

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
 export const KEYWORDS = ["say", "love", "as", "a"] as const;
 
 export const VARIABLE_TYPES = ["string", "number"] as const;
+
+// default values
+export const DEFAULT_VARIABLES_VALUES = {
+  number: 0,
+  string: null,
+} as const;
