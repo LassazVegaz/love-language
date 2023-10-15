21a7910ae1c2d166ce246bf818ca930089407225
LassazVegaz <lasindunuwanga@gmail.com>
2023-10-15 23:34:05 +0530

    EOL const

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
-export const KEYWORDS = ["say", "love", "as", "a", "\n"] as const;
+// EOL is end of line. This is used instead of \n or \r\n because it they can be different on
+// different operating systems. Here I just want to identify the end of a line. When the tokenizer
+// finds \n or \r\n, it will replace them with EOL.
+export const EOL = "EOL" as const;
+
+export const KEYWORDS = ["say", "love", "as", "a", EOL] as const;
 
 export const VARIABLE_TYPES = ["string", "number"] as const;
 
